4b51afc3b04a6e68edd4a6fc303e8d0b494c5c09
Richard Bradley <Richard.Bradley@softwire.com>
2015-08-06 18:06:57 +0100

    Add note about "provided" scope for inspections dependency

--- project/Build.scala
+++ project/Build.scala
@@ -13,14 +13,24 @@ object ScapegoatCustomInspectionsExample extends Build {
     .settings(
       scalaVersion := scalaV,
       scapegoatCustomInspections := List(
-        "msw.mpaf.inspections.logging.LoggingArgumentsMismatch"),
+        "my.inspections.DisallowJavaDate"),
       scapegoatCustomInspectionsClasspath := List(
-        new File("inspections/target/scala-2.11/classes/").getAbsoluteFile))
+        new File("inspections/target/scala-2.11/scapegoat-classes/").getAbsoluteFile))
+    // FIXME: this dependency ought to be in scope 'provided', as it is
+    // compile only, so is not needed at runtime.
+    // Unfortunately, the current definition of the SBT 'scapegoat' task
+    // includes a "clean", which means that SBT will delete the compiled
+    // inspection classes just before attempting to run the inspections.
+    // If the dependency here is "compile" then SBT knows to rebuild the
+    // inspections before running them, however it does not add that
+    // task dependency if this is marked "provided". I think that is a
+    // bug in SBT.
+    .dependsOn(inspections)
 
   lazy val inspections = Project("inspections", file("inspections"))
     .settings(
       libraryDependencies ++= List(
         "com.sksamuel.scapegoat" %% "scalac-scapegoat-plugin" % "1.1.0",
-        "org.scala-lang" % "scala-compiler" % scalaV % "provided"),
+        "org.scala-lang" % "scala-compiler" % scalaV),
       scalaVersion := scalaV)
 }
